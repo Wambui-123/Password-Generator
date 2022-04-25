5e4167d4e0aa749fcd048205a776fe70d09fa61d
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-04-25 22:30:42 +0300

    adding functionality

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 from credentials import Credential
 from credentials import User
 
+
 def main():
     print(' ')
 print('Hello! Welcome to Password Locker.')
@@ -16,53 +17,55 @@ elif short_code == 'sp':
 elif short_code == 'li':
 			print(' ')
 			print('To login, enter your account details:')
-			username = input('Enter your first name - ').strip()
-			password = str(input('Enter your password - '))
-user_exists = (username,password)     
-if user_exists == User.username:
-        print(f'Welcome {username}. Please choose an option to continue.')
+user = input('Enter your first name - ').strip()
+password = str(input('Enter your password - '))
+user_exists = (user,password)     
+if user_exists == user:
+        print(f'Welcome {user}. Please choose an option to continue.')
         print(' ')
 else: 
     print(f'You do not have an account' "\n" 'Please Sign Up') 
     print('Use these codes to navigate: \n sp-Sign Up \n li-Log In \n ex-Exit')
 # while True:
 print("-"*60)
-# print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
-# 	short_code = input('Enter a choice: ').lower().strip()
-# 	print("-"*60)
+print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
+short_code = input('Enter a choice: ').lower().strip()
+print("-"*60)
 if short_code == 'ex':
     print(" ")
-    print(f'Goodbye {username}')
-      
-		# 				break
-		# 			elif short_code == 'cc':
-		# 				print(' ')
-		# 				print('Enter your credential details:')
-		# 				site_name = input('Enter the site\'s name- ').strip()
-		# 				account_name = input('Enter your account\'s name - ').strip()
-		# 				while True:
-		# 					print(' ')
-		# 					print("-"*60)
-		# 					print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
-		# 					psw_choice = input('Enter an option: ').lower().strip()
-		# 					print("-"*60)
-		# 					if psw_choice == 'ep':
-		# 						print(" ")
-		# 						password = input('Enter your password: ').strip()
-		# 						break
-		# 					elif psw_choice == 'gp':
-		# 						password = generate_password()
-		# 						break
-		# 					elif psw_choice == 'ex':
-		# 						break
-		# 					else:
-		# 						print('Oops! Wrong option entered. Try again.')
-		# 				save_credential(create_credential(user_name,site_name,account_name,password))
-		# 				print(' ')
-		# 				print(f'Credential Created: Site Name: {site_name} - Account Name: {account_name} - Password: {password}')
-		# 				print(' ')
-		# 			elif short_code == 'dc':
-		# 				print(' ')
+    print(f'Goodbye {user}')
+from run import User
+
+
+# break
+# elif short_code == 'cc':
+# print(' ')
+# print('Enter your credential details:')
+# site_name = input('Enter the site\'s name- ').strip()
+# account_name = input('Enter your account\'s name - ').strip()
+# while True:
+# print(' ')
+# print("-"*60)
+# print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
+# psw_choice = input('Enter an option: ').lower().strip()
+# print("-"*60)
+# if psw_choice == 'ep':
+# 	print(" ")
+# 	password = input('Enter your password: ').strip()
+# 	break
+# elif psw_choice == 'gp':
+#     password = generate_password()
+# break
+# elif psw_choice == 'ex':
+# 	break
+# 		else:
+# 		print('Oops! Wrong option entered. Try again.')
+# 		save_credential(create_credential(user_name,site_name,account_name,password))
+# 		print(' ')
+# 		print(f'Credential Created: Site Name: {site_name} - Account Name: {account_name} - Password: {password}')
+# 		print(' ')
+# 		elif short_code == 'dc':
+# 						print(' ')
 # if display_credentials(user_name):
 #     print('Here is a list of all your credentials')
 # print(' ')
@@ -80,14 +83,14 @@ if short_code == 'ex':
 # print('')
 
 
-		# 	else: 
-		# 		print(' ')
-		# 		print('Oops! Wrong details entered. Try again or Create an Account.')		
+# 			else: 
+# 				print(' ')
+# 				print('Oops! Wrong details entered. Try again or Create an Account.')		
 		
-		# else:
-		# 	print("-"*60)
-		# 	print(' ')
-		# 	print('Oops! Wrong option entered. Try again.')
+# 		else:
+# 			print("-"*60)
+# 			print(' ')
+# 			print('Oops! Wrong option entered. Try again.')
 				
 # else short_code == "":
 # print('Oops! Wrong option entered. Try again.')
