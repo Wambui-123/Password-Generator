d8296f287b31379b030da165cb0f95ee4218a7b0
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-04-25 21:14:43 +0300

    adding functionality

---
+++ run.py
@@ -0,0 +1,41 @@
+from password_generator import PasswordGenerator
+import random
+
+pwo = PasswordGenerator()
+pwo.generate()
+pwo.minlen = 6
+pwo.maxlen = 12 
+
+class User :
+    	
+    def __init__(self,name,username,generate,password,length):
+        self.name = name
+        self.username = username
+        self.generate = generate
+        self.password = password
+        self.length = length
+name = input("Your name: ")               
+username = input("Enter Your Username: ")
+generate = (pwo.generate())
+print("Suggested password: " + generate)
+print("Would you want to use this suggested password? " +'\n' "Type 'Yes' or 'No'" ) 
+
+
+chosen_element= input()
+if str(chosen_element) == 'Yes':
+    print(name + " your username is " + username + " and your password is: " + generate)
+elif str(chosen_element) == 'No':
+    password = input("Enter in your password: " )
+    if len(password) < pwo.minlen:
+        print('Invalid password, password must atleast contain six characters') 
+        length= int(len(password))
+        print("Your password contains: " + str(length) + " characters " + '\n') 
+        print("Type another password")
+        password = input("Enter in your password: " )
+    else: 
+     print(username + " your have successfully signed up.")
+    print(name + " your username is " + username + " and your password is: " + password) 
+
+
+
+
--- user.py
+++ user.py
@@ -1,47 +1,100 @@
-from collections import UserList
-from password_generator import PasswordGenerator
-import random
+from credentials import Credential
+from credentials import User
 
-pwo = PasswordGenerator()
-pwo.generate()
-pwo.minlen = 6
-pwo.maxlen = 12 
+def main():
+    print(' ')
+print('Hello! Welcome to Password Locker.')
+# while True:
+print('Use these codes to navigate: \n sp-Sign Up \n li-Log In \n ex-Exit')
+short_code = input('Enter a choice: ').lower().strip()
+if short_code == 'ex': 
+    exit()
+elif short_code == 'sp':
+			# print('To create a new account:')
+                from run import User
+                print(" ")
+elif short_code == 'li':
+			print(' ')
+			print('To login, enter your account details:')
+			username = input('Enter your first name - ').strip()
+			password = str(input('Enter your password - '))
+user_exists = (username,password)     
+if user_exists == User.username:
+        print(f'Welcome {username}. Please choose an option to continue.')
+        print(' ')
+else: 
+    print(f'You do not have an account' "\n" 'Please Sign Up') 
+    print('Use these codes to navigate: \n sp-Sign Up \n li-Log In \n ex-Exit')
+# while True:
+print("-"*60)
+# print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
+# 	short_code = input('Enter a choice: ').lower().strip()
+# 	print("-"*60)
+if short_code == 'ex':
+    print(" ")
+    print(f'Goodbye {username}')
+      
+		# 				break
+		# 			elif short_code == 'cc':
+		# 				print(' ')
+		# 				print('Enter your credential details:')
+		# 				site_name = input('Enter the site\'s name- ').strip()
+		# 				account_name = input('Enter your account\'s name - ').strip()
+		# 				while True:
+		# 					print(' ')
+		# 					print("-"*60)
+		# 					print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
+		# 					psw_choice = input('Enter an option: ').lower().strip()
+		# 					print("-"*60)
+		# 					if psw_choice == 'ep':
+		# 						print(" ")
+		# 						password = input('Enter your password: ').strip()
+		# 						break
+		# 					elif psw_choice == 'gp':
+		# 						password = generate_password()
+		# 						break
+		# 					elif psw_choice == 'ex':
+		# 						break
+		# 					else:
+		# 						print('Oops! Wrong option entered. Try again.')
+		# 				save_credential(create_credential(user_name,site_name,account_name,password))
+		# 				print(' ')
+		# 				print(f'Credential Created: Site Name: {site_name} - Account Name: {account_name} - Password: {password}')
+		# 				print(' ')
+		# 			elif short_code == 'dc':
+		# 				print(' ')
+# if display_credentials(user_name):
+#     print('Here is a list of all your credentials')
+# print(' ')
+# for credential in display_credentials(user_name):
+#     print(f'Site Name: {credential.site_name} - Account Name: {credential.account_name} - Password: {credential.password}')
+#     print(' ')	
+# else:
+#     print(' ')
+#     print("You don't seem to have any credentials saved yet")
+#     print(' ')
+# elif short_code == 'copy':
+# print(' ')
+# chosen_site = input('Enter the site name for the credential password to copy: ')
+# copy_credential(chosen_site)
+# print('')
 
-class User :
-    	
-    def __init__(self,username,generate,password,length):
-        self.username = username
-        self.generate = generate
-        self.password = password
-        self.length = length
-               
-username = input("Enter Your Username: ")
-generate = (pwo.generate())
-print("Suggested password: " + generate)
-print("Would you want to use this suggested password? " +'\n' "Type 'Yes' or 'No'" ) 
+
+		# 	else: 
+		# 		print(' ')
+		# 		print('Oops! Wrong details entered. Try again or Create an Account.')		
+		
+		# else:
+		# 	print("-"*60)
+		# 	print(' ')
+		# 	print('Oops! Wrong option entered. Try again.')
+				
+# else short_code == "":
+# print('Oops! Wrong option entered. Try again.')
 
 
-chosen_element= input()
-if str(chosen_element) == 'Yes':
-    print(username + " your password is: " + generate)
-elif str(chosen_element) == 'No':
-    password = input("Enter in your password: " )
-    if len(password) < pwo.minlen:
-        print('Invalid password, password must atleast contain six characters') 
-        length= int(len(password))
-        print("Your password contains: " + str(length) + " characters " + '\n') 
-        print("Type another password")
-        password = input("Enter in your password: " )
-    else: 
-     print(username + " your have successfully logged in.")
-    print("Your password is " + password)        
-    
-    
-def main(): 
-        f = open('user.txt', 'a')
-        f.write("Account Details " + '\n')
-        f.write("Username: " + username + '\n')
-        f.write("Password: " + password + '\n')  
-        f.close()   
-        main()    
 
+
+
+if __name__ == '__main__':
+	main()
